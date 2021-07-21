b3b029e83ed885ba73920339b3b8937b783aff8f
harxzinc <harxzmail@gmail.com>
2021-07-21 11:34:10 -0500

    Esta es una ruta

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 
 const routes = [
@@ -10,7 +10,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  mode: 'history',
+  history: createWebHashHistory(),
   routes
 })
 
